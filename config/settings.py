5aab9392f1ec6d5b289289bd5134704faf21f849
Andri Janusson <andrijan@gmail.com>
2019-06-26 13:20:38 +0000

    Add django-storages

--- config/settings.py
+++ config/settings.py
@@ -134,5 +134,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
+
+AWS_ACCESS_KEY_ID = 'AKIAJMWO43BAUFTODJTQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'fiver-media'
+AWS_S3_FILE_OVERWRITE = False
